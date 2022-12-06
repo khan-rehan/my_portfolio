b7eff21784fc6ca4686d28913f5f4f55913d0701
CasperRehan <rehankhancr25@gmail.com>
2022-12-06 08:37:18 +0530

    commented star and fork for BC repo

--- src/components/footer.js
+++ src/components/footer.js
@@ -109,7 +109,7 @@ const Footer = () => {
             Built By Khan Rehan
           </div>
 
-          {githubInfo.stars && githubInfo.forks && (
+          {/* {githubInfo.stars && githubInfo.forks && (
             <StyledGitHubInfo>
               <span>
                 <FormattedIcon name="Star" />
@@ -120,7 +120,7 @@ const Footer = () => {
                 <span>{githubInfo.forks.toLocaleString()}</span>
               </span>
             </StyledGitHubInfo>
-          )}
+          )} */}
         </StyledGitHubLink>
       </StyledMetadata>
     </StyledContainer>
